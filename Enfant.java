3c28fff0920bfb0973d660d06bc30f0aa7070c00
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-04 11:26:06 +0200

    ajout même méthodes que pour Personne

--- Enfant.java
+++ Enfant.java
@@ -17,5 +17,32 @@ public class Enfant { //protected car on a des sous classes d'Enfant
 		this.deptNaissance=deptNaissance;
 	}
 
-	// mêmes méthodes qu'avec Personne?
+	public void toString(Personne p) {
+		System.out.print("Le nom est "+p.nom+". Le prénom est "+p.prenom+".");
+		if (p.sexe!='N')
+			System.out.print(" Le sexe est "+p.sexe+".");
+		if (p.anneeNaissance!=0)
+			System.out.print(" L'année de naissance est "+p.anneeNaissance+".");
+		if (p.deptNaissance!=0)
+			System.out.print(" Le département de naissance est "+p.deptNaissance+".");
+	}
+
+	
+	
+	public String getNom() {return this.nom; }
+	public String getPrenom() {return this.prenom;}
+	public char getSexe() {return this.sexe;}
+	public String getNumSecu() {return this.numSecu;}
+	public int getAnneeNaissance() {return this.anneeNaissance;}
+	public int getDeptNaissance() {return this.deptNaissance;}
+	public String getAdresse() {return this.adresse;}
+ 
+	public void setNom(String nom) {        this.nom=nom;}
+    	public void setPrenom(String prenom) {this.prenom=prenom;}
+    	public void setSexe(char sexe) {this.sexe=sexe;}
+	public void setNumSecu(String numSecu) {
+		this.numSecu=numSecu;
+		this.anneeNaissance=anneeNaissance(numSecu);
+		this.deptNaissance=deptNaissance(numSecu);
+	}
 }
