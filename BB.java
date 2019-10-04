a88b511e7c96931ae27f535a7490b97f3b6f303d
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-04 11:18:51 +0200

    comm supprimé

--- BB.java
+++ BB.java
@@ -2,7 +2,6 @@ package projetjava;
 
 public class BB extends Enfant {
 
-	//adresse
 	public BB(String nom, String prenom, int anneeNaissance, char sexe, int deptNaissance, String adresse) {
 		super(nom, prenom, anneeNaissance, sexe, deptNaissance, adresse);
 	}
