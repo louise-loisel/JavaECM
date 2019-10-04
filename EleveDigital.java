3edf76bf360fb86df3c8eb8917777b3e08a0049a
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-04 11:31:46 +0200

    private varinst

--- EleveDigital.java
+++ EleveDigital.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class EleveDigital extends Eleve {
     
-	protected ArrayList<Note> listeNotes;
+	private ArrayList<Note> listeNotes;
 	public EleveDigital(String nom, String prenom, String numSecu, int anneeNaissance, char sexe, int deptNaissance, String adresse, int ine, int promo, int annee){
 		super(nom, prenom, numSecu, ine, promo, annee, adresse);
 		this.listeNotes= new ArrayList<Note>();
