c5d57931ed1abbd1940e0c3c56082d0b4031ec36
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-04 11:29:24 +0200

    var protected

--- Eleve.java
+++ Eleve.java
@@ -1,7 +1,7 @@
 package projetjava;
 
 public class Eleve extends Personne {
-    int ine, promo, annee;
+    protected int ine, promo, annee;
 
     public Eleve(String nom, String prenom, String numSecu, int ine, int promo, int annee, String adresse){
         super(nom, prenom, numSecu, adresse);
