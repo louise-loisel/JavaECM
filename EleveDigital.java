1220edb9f0a5ed1c6acd03d58fbc16bb62307f8d
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-04 23:56:25 +0200

    constructeur simplifié (Personne+Eleve+EleveDigi)

--- EleveDigital.java
+++ EleveDigital.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class EleveDigital extends Eleve {
     
 	private ArrayList<Note> listeNotes;
-	public EleveDigital(String nom, String prenom, String numSecu, int anneeNaissance, char sexe, int deptNaissance, String adresse, int ine, int promo, int annee){
+	public EleveDigital(String nom, String prenom, String numSecu, String adresse, int ine, int promo, int annee){
 		super(nom, prenom, numSecu, ine, promo, annee, adresse);
 		this.listeNotes= new ArrayList<Note>();//a-t-on besoin du this?
 	}
