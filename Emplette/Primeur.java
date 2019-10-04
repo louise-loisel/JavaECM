f5d616ecbb9ddaf5337bf162f27aeb8d010d86f0
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-04 15:22:30 +0200

    interf implements

--- Emplette/Primeur.java
+++ Emplette/Primeur.java
@@ -1,5 +1,8 @@
 package projetjava;
 
-public class Primeur extends Article{
+public class Primeur extends Article implements IPublicite {
 
+    public void slogan(){
+      System.out.println("Venez vous nourrir yes !");
+  }
 }
