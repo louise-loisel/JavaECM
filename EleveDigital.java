e7ee79eecd5b7327153abed0f14d3237a30a8b19
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-04 14:08:52 +0200

    add instructions

--- EleveDigital.java
+++ EleveDigital.java
@@ -1,17 +1,25 @@
 package projetjava;
 import java.util.*;
+//Créer la classe EleveDigital qui a en plus un ArrayList pour gérer ses notes.Donner les constructeurs et les méthodes pour gérer ceci correctement.
+
 
 public class EleveDigital extends Eleve {
     
 	private ArrayList<Note> listeNotes;
 	public EleveDigital(String nom, String prenom, String numSecu, int anneeNaissance, char sexe, int deptNaissance, String adresse, int ine, int promo, int annee){
 		super(nom, prenom, numSecu, ine, promo, annee, adresse);
-		this.listeNotes= new ArrayList<Note>();
+		this.listeNotes= new ArrayList<Note>();//a-t-on besoin du this?
 	}
 	
 	public void ajouteNote(Note n) {
-		this.listeNotes.add(n);
+		this.listeNotes.add(n); //a-t-on besoin du this?
 	}
+	
+    	public ArrayList<Note> getNotes() {
+        	return notes;
+    	}
+
+
 	
 
 }
