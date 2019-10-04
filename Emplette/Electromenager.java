c219073a53e7dd057d7fa668f0d805f48aacc8d8
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-04 15:22:40 +0200

    interf implements

--- Emplette/Electromenager.java
+++ Emplette/Electromenager.java
@@ -1,5 +1,9 @@
 package projetjava;
 
-public class Electromenager extends Article {
+public class Electromenager extends Article implements IPublicite {
 
+  
+  public void slogan(){
+    System.out.println("Venez vous équipez pour la maison yes !");
+  }
 }
