fcfee6c1cb6e5b958854223d6258569b7c9a67a6
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-05 13:39:18 +0200

    getTaux, getFixe

--- Iatos.java
+++ Iatos.java
@@ -2,7 +2,7 @@ package projetjava;
 
 public class Iatos extends Personnel{
     // pas de variables déclarées ici?
-    public iatos(String nom, String prenom, String numSecu, String adresse, String date_entree, int tempsTravail, float nbHeuresSupp, float taux){
+    public iatos(String nom, String prenom, String numSecu, String adresse, String date_entree, int tempsTravail, int nbHeuresSupp, float taux){
         super(nom,prenom,numSecu,adresse,date_entree,tempsTravail);
         // est ce que super appelle le constructeur de Personnel qui met des valeurs par défaut à nbHeuresSupp, fixe et taux?
         // ou est ce que super appelle les variables d'instance? dans ce cas nbHeuresSupp, fixe et taux ne sont pas initialisées par super
@@ -16,14 +16,15 @@ public class Iatos extends Personnel{
     public float calculSalaire(){
         return(this.fixe*(this.tempsTravail/100)+this.nbHeuresSupp*this.taux);
     }
-    //ai modifié : avant on faisait fixe*(tempsTravail/100) + nbHeuresSupp*taux
-    //mais je pense pas que 1200 soit un salaire horaire, donc pourquoi diviser par 100?
-    // Surtout que pour l'instant, sa valeur par défaut dans le constructeur de Personnel c'est 0.
 
+    public float getTaux(){ return this.taux;}
+    public float getFixe(){ return this.fixe;}
+    
     public void setTaux(int taux) {
         this.taux = taux;
     }
     public void setFixe(int fixe) {
         this.fixe = fixe;
     }
+    
 }
