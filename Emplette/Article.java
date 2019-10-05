84f19fe2011348e3447cbf7428439ca1977dc158
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-05 17:55:26 +0200

    Update Article.java

--- Emplette/Article.java
+++ Emplette/Article.java
@@ -1,11 +1,14 @@
 package projetjava;
 
-public class Article {
+public class Article implements IVendrePiece, IVendreKilo {
 	protected String nom;
 	protected float prixAchat;
 	protected float prixVente;
 	protected float tauxSoldes;
 		protected boolean remboursable;
+		protected float qteStock;
+		protected String pieceKilo;
+		protected boolean enStock;
 	
 	public Article(String nom, float prixAchat, float prixVente) {
 		this.nom=nom;
@@ -13,7 +16,59 @@ public class Article {
 		this.prixVente=prixVente;
 		this.tauxSoldes=0;
 		this.remboursable=false;
+		this.quantiteStock=0;
+		this.pieceKilo="default";
+		if (quantiteStock>0;){this.enStock = true};
 	}
 	
-	public boolean getRemboursable(){return this.remboursable;};
+	public boolean getRemboursable(){return this.remboursable;}
+	public float getQteStock(){return this.qteStock;}
+	
+	// méthodes vente
+	public boolean vente();
+	public boolean remboursement();
+	
+	public void vendrePiece(int qteVente){
+		if (this.enStock && qteVente<this.qte Stock;)
+			{this.qteStock-=qteVente;
+			System.out.println("Article vendu, il reste " + this.qteStock + " en stock.");
+			boolean vente=true;
+			}
+		else if (this.enStock && qteVente=this.qteStock;)
+			{this.qteStock=0;
+			this.enStock=false;
+			System.out.println("Article vendu, il n'en reste plus en stock.");
+			boolean vente=true;
+			}
+		else if (!this.enStock || qteVente>this.qteStock)
+			{System.out.println("Pas assez d'article en stock")
+			boolean vente=false;}
+	};
+	
+	
+	public void vendreKilo(float qteVente){
+		if (this.enStock && qteVente<this.qte Stock;)
+			{this.qteStock-=qteVente;
+			System.out.println("Article vendu, il reste " + this.qteStock + " en stock.");
+			boolean vente=true;
+			}
+		else if (this.enStock && qteVente=this.qteStock;)
+			{this.qteStock=0;
+			this.enStock=false;
+			System.out.println("Article vendu, il n'en reste plus en stock.");
+			boolean vente=true;
+			}
+		else if (!this.enStock || qteVente>this.qteStock)
+			{System.out.println("Pas assez d'articles en stock")
+			boolean vente=false;}
+	};
+	
+	
+	// getters & setters
+	
+	public void setEnStock(boolean enStock){this.enStock=enStock;}
+	public void ajoutQteStock(float ajout){this.qteStock+=ajout;}
+	
+	
+	
 }
