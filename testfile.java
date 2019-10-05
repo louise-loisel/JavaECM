8a85fd55f8d41ba9b2cec71ce002cbeb337479c9
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 23:14:27 +0200

    Update testfile.java

--- testfile.java
+++ testfile.java
@@ -7,36 +7,50 @@ public class testfile {
         // ***** Vérification des classes Personne, CompteBanq
 
         Personne Default = new Personne("Jane", "Doe");
-
-        Personne Aimée = new Personne("Aimée", "Boinnot", "296750000000000", "adresse");
-        System.out.println("sexe Aimée = "+Aimée.getSexe());
-        System.out.println("année naiss Aimée = " + Aimée.getAnneeNaissance());
-        System.out.println("age Aimée = " + calculAge(Aimée));
-        System.out.println("dept Aimée = " + Aimée.getDeptNaissance()); // problème avec le département de naissance, retourne '0'?
-        Aimée.setNumSecu("105780000000000");
+        
+        //Test Personne Aimée
+        Personne Aimée = new Personne("Aimée", "Boinnot", "296753700000000", "adresse");
         System.out.println("sexe Aimée = " + Aimée.getSexe());
         System.out.println("année naiss Aimée = " + Aimée.getAnneeNaissance());
-        System.out.println("age Aimée = " + Aimée.calculAge());
-        System.out.println("dept Aimée = " + Aimée.getDeptNaissance()); // problème avec le département de naissance, retourne '0'?
+        System.out.println("age Aimée = " + Personne.calculAge(Aimée));
+        System.out.println("dept Aimée = " + Aimée.getDeptNaissance()); // 
+        Aimée.setNumSecu("105781300000000");
+        System.out.println("sexe Aimée = " + Aimée.getSexe());
+        System.out.println("année naiss Aimée = " + Aimée.getAnneeNaissance());
+        System.out.println("age Aimée = " + Personne.calculAge(Aimée));
+        System.out.println("dept Aimée = " + Aimée.getDeptNaissance());
+        
+        /*
+CONSOLE
+sexe Aimée = F
+année naiss Aimée = 1996
+age Aimée = 23
+dept Aimée = 37
+sexe Aimée = M
+année naiss Aimée = 2005
+age Aimée = 14
+dept Aimée = 13
+        */
+        
         Personne Alice = new Personne("Alice", "Howard", "295920000000000", "adresse");
         CompteBanq AliceBanque = new CompteBanq("125", Alice, "123", "234");
         System.out.println("solde Alice défaut= " + AliceBanque.getSolde()); // solde par défaut = 0
         System.out.println("découvert autorisé Alice défaut= " + AliceBanque.getDecouvertAutorise()); // découvert autorisé par défaut
-        System.out.println("versement mauvais code")
+        System.out.println("versement mauvais code");
         AliceBanque.versement(100, "000"); // tentative versement mauvais code
-        System.out.println("versement bon code")
+        System.out.println("versement bon code");
         AliceBanque.versement(100, "123"); // versement code correct
         System.out.println("verification bon versement, solde alice =" + AliceBanque.getSolde());// vérification bon versement
-        System.out.println(" retrait mauvais code, trop découvert")
+        System.out.println(" retrait mauvais code, trop découvert");
         AliceBanque.retrait(25, "000");// retrait mauvais code, trop découvert
-        System.out.println(" retrait bon code, trop découvert")
+        System.out.println(" retrait bon code, trop découvert");
         AliceBanque.retrait(200, "123");// retrait bon code, trop découvert
-        System.out.println("retrait -25 bon code, pas de découvert")
+        System.out.println("retrait -25 bon code, pas de découvert");
         AliceBanque.retrait(25, "123");// retrait bon code, pas de découvert
         // premier retrait 25
-        System.out.println("augmentation découvert + 50, mauvais code")
+        System.out.println("augmentation découvert + 50, mauvais code");
         AliceBanque.setDecouvertAutorise(50, "000"); // augmentation découvert, mauvais code
-        System.out.println("augmentation découvert + 50, bon code")
+        System.out.println("augmentation découvert + 50, bon code");
         AliceBanque.setDecouvertAutorise(50, "234");// augmentation découvert, bon code
         System.out.println("Vérification que le découvert a bien fonctionné");
         System.out.println(AliceBanque.getDecouvertAutorise());
