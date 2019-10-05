cd787f489d4b72e6aaa73fc80f1852ccee4c29c8
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-05 22:17:41 +0200

    Update Magasin.java

--- Emplette/Magasin.java
+++ Emplette/Magasin.java
@@ -5,54 +5,72 @@ public class Magasin implements IVendrePiece, IVendreKilo{
 	protected String nom;
 	protected String adresse;
 	protected int nbVendeurs;
-	protected ArrayList<Article> articlesEnStock;
-//	protected ArrayList<Float> qteArticlesEnStock; 
-// la quantité en stock est rentrée comme attribut de l'article
+	protected ArrayList<Article> articlesVendus; // la quantité en Stock est rentrée dans les attributs de l'article
 	protected float caisse;
 	
-	public Magasin(String nom, String adresse, int nbVendeurs, float caisse) {
+	public Magasin(String nom, String adresse, int nbVendeurs, float caisse, ArrayList<Article> articlesMagasin ) {
 		this.nom=nom;
 		this.adresse=adresse;
 		this.nbVendeurs=nbVendeurs;
 		this.caisse=caisse;
-		this.articlesEnStock = new ArrayList<Article>();
-//		this.qteArticlesEnStock = new ArrayList<Float>();
+		this.articlesVendus = articlesMagasin;
 	}
 	
-/*	public void achatArticles(Article art, float qte){
-		if art.prixAchat*qte<this.caisse{
-			articlesEnStock.add(art);
-			qteArticlesEnStock(qte);
-			this.caisse-=art.prixAchat*qte;
+	public boolean enVente(Article art){ // booléen pour voir si article demandé dans la liste des articles du magasin
+		for(Article a : this.articlesVendus){
+			if (a instanceof art) 
+				return true;
 		}
-		else{
-			System.out.println("Fonds du magasin insuffisants");
+	}
+	
+	public int indexArticle(String nomArticle) { // rend l'indice de l'article du nom recherché dans la liste articlesVendus
+	    for(var i = 0; i < articlesVendus.length; i += 1) {
+	        if(articlesVendus[i].getNom().equals(nomArticle)) {return i;}
+    		}
+    		return -1;
 		}
-*/
-		
+	
+	public void achat(Article art, float qteAchat){
+		if art.prixAchat*qteAchat<=this.caisse && this.enVente(Article art); 
+			{System.out.println("Achat possible, suffisamment d'argent en caisse");
+			
+			// trouver article correspondant (suivant le nom !) dans la liste et incrémenter sa quantité
+			//+ update boolean enStock
+			int index = this.indexArticle(String art.getNom());
+			articlesVendus[index].ajoutQteStock(float qteAchat);
+			articlesVendus[index].artEnStock();
+			
+			this.caisse-=art.prixAchat*qteAchat;}
+		else if (!this.enVente( Article art)){
+			System.out.println("L'article n'est pas vendu dans ce magasin");};
+		else if (art.prixAchat*qteAchat>this.caisse){System.out.println("Pas assez d'argent en caisse pour cet achat");}
+		}
+
+			 
+	public void vendre(Article art, float qteVente){
+		/* on pourrait faire art.vendreKilo(), mais je ne sais pas si modifier le stock de quantité de l'instance art
+		modifiera aussi sa valeur dans l'array articlesVendus. Je ne sais pas si la valeur des attributs est automatiquement
+		mise-à-jour dans la liste d'articles. */
+		int index = this.indexArticle(String art.getNom())
+		if art.pieceKilo.equals("kilo");
+			articlesVendus[index].vendreKilo(float qteVente);// devrait normalement gérer la quantité en stock si la vente a lieu
+		else if art.pieceKilo.equals("piece") && ((int)qteVente == qteVente); // check que la quantité rentrée est bien entière
+			articlesVendus[index].vendrePiece((int)qteVente);	// convertir le float rentré en int lorsqu'on l'intègre à l'argument de la fonction
+		else if art.pieceKilo.equals("piece") && (!(int)qteVente == qteVente);
+			System.out.println("Erreur : le nombre de pièces demandé n'est pas entier";);
+		if art.vente();
+			this.caisse+=art.prixVente*qteVente; 
 	}
 
-	
-	public void vendreArticle(Article art) // argument = item de la liste d'articlesEnStock
-		if art.kiloPiece="kilo";
-			art.vendrekilo(); // devrait normalement gérer la quantité en stock si la vente a lieu
-		if art.kiloPiece="piece";
-			art.vendrePiece();
-		if art.vente; 
-			this.caisse+=art.prixVente; 
-	
+
 	public void retourArticle(Article art, boolean etatRetour){
-		if (art.remboursable && etatRetour;){
-			this.caisse-=art.prixVente;
-			art.ajoutQteStock(1); // on ne retourne qu'un article à la fois (vérification du bon état)
-			art.setEnStock(true);
+		this.caisse-=art.prixVente;
+		if (art.pieceKilo.equals("piece") && etatRetour;){ // dans le cas d'un article type "piece" on peut le remettre en vente
+			
+			int index = this.indexArticle(String art.getNom());
+			articlesVendus[index].ajoutQteStock(1);// on ne retourne qu'un article type "piece" à la fois (vérification du bon état)
+			articlesVendus[index].artEnStock();}
 		}
-		else if (art.remboursable && !etatRetour){
-			this.caisse-=art.prixVente;
-		}
-		else if (!art.remboursable){
-			System.out.println("Pas de retour possible");
-		}
-	};
+	}
 	
 }
