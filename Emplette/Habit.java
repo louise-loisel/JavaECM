fd11296c5492da8449d9bc744d3c22c2ef8f1959
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 14:20:20 +0200

    def soldes

--- Emplette/Habit.java
+++ Emplette/Habit.java
@@ -1,9 +1,38 @@
 package projetjava;
 
-public class Habit extends Article implements IPublicite  {
+public class Habit extends Article implements IPublicite, IVendrePiece, ISolde  {
+    //constructeur
+    public Habit(String nom, float prixAchat, float prixVente){
+       super(nom, prixAchat, prixVente);
+      }
   
+  
+  //pub
     public void slogan(){
-    System.out.println("Venez vous rhabiller yes !");
-  }
+      System.out.println("Venez vous rhabiller yes !");
+    }
 
+    //connaitre taux soldes
+    public void tauxSoldes(){
+      System.out.println("Le taux de soldes est de "+(tauxSoldes*100)+"%.");
+    }
+  
+    //lancer soldes
+    public void lancerSoldes(float taux){
+      throws  PasEntre0et100ExceptionA
+    {  
+      if(0<taux<1)
+      {
+        this.tauxSoldes=taux;
+      }
+      else
+      {
+        throw new PasEntre0et100ExceptionA();
+      }
+  }
+    
+  //stopper soldes
+  public void stopSoldes(){
+    this.taux=0;
+  }
 }
