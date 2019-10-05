f23824e4004de11e6bb3f8fa50d560f63ca0aff3
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 14:16:19 +0200

    Update IVendrePiece.java

--- Emplette/IVendrePiece.java
+++ Emplette/IVendrePiece.java
@@ -1,7 +1,7 @@
 package projetjava;
 
 public interface IVendrePiece {
-	public void VendrePiece();
-	public void RembourserPiece();
+	public void vendrePiece();
+	public void rembourserPiece();
 
 }
