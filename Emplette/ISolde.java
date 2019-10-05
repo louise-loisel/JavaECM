f45391ff6f2785129e0d2570924a6821cd972c8e
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 14:15:37 +0200

    Update ISolde.java

--- Emplette/ISolde.java
+++ Emplette/ISolde.java
@@ -1,7 +1,7 @@
 package projetjava;
 
 public interface ISolde {
-	public void TauxSoldes();
-	public void LancerSoldes(float taux);
-	public void StopSoldes();
+	public void tauxSoldes();
+	public void lancerSoldes(float taux);
+	public void stopSoldes();
 }
