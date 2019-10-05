c754eac65e4dbdd01ff90e57d818739dfa22911a
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 14:21:58 +0200

    soldes

--- Emplette/Primeur.java
+++ Emplette/Primeur.java
@@ -1,8 +1,35 @@
 package projetjava;
 
-public class Primeur extends Article implements IPublicite {
-
+public class Primeur extends Article implements IPublicite, IVendreKilo, ISolde  {
+    //constructeur
+    public Primeur(String nom, float prixAchat, float prixVente){
+       super(nom, prixAchat, prixVente);
+      }
     public void slogan(){
       System.out.println("Venez vous nourrir yes !");
+        
+        //connaitre taux soldes
+    public void tauxSoldes(){
+      System.out.println("Le taux de soldes est de "+(this.tauxSoldes*100)+"%.");
+    }
+  
+    //lancer soldes
+    public void lancerSoldes(float taux){
+      throws  PasEntre0et100ExceptionA
+    {  
+      if(0<taux<1)
+      {
+        this.tauxSoldes=taux;
+      }
+      else
+      {
+        throw new PasEntre0et100ExceptionA();
+      }
+  }
+    
+  //stopper soldes
+  public void stopSoldes(){
+    this.taux=0;
+  }
   }
 }
