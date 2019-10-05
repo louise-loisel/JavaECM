3654e8f8ac2cf5cfa925d50633b3451ab1b51dbf
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-05 15:46:26 +0200

    ajout bool remboursable

--- Emplette/Article.java
+++ Emplette/Article.java
@@ -4,12 +4,14 @@ public class Article {
 	protected String nom;
 	protected float prixAchat;
 	protected float prixVente;
-	protected float tauxSoldes
+	protected float tauxSoldes;
+		protected boolean remboursable;
 	
-	public Article(String nom, float prixAchat, float prixVente) {
+	public Article(String nom, float prixAchat, float prixVente,boolean remboursable) {
 		this.nom=nom;
 		this.prixAchat=prixAchat;
 		this.prixVente=prixVente;
 		this.tauxSoldes=0;
+		this.remboursable=remboursable;
 	}
 }
