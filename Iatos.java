d27ff447387151ebc77fff0cde82c57c82a507b7
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 19:20:55 +0200

    Update Iatos.java

--- Iatos.java
+++ Iatos.java
@@ -2,7 +2,7 @@ package projetjava;
 
 public class Iatos extends Personnel{
     // pas de variables déclarées ici?
-    public iatos(String nom, String prenom, String numSecu, String adresse, String date_entree, int tempsTravail, int nbHeuresSupp, float taux){
+    public Iatos(String nom, String prenom, String numSecu, String adresse, String date_entree, int tempsTravail, int nbHeuresSupp, float taux){
         super(nom,prenom,numSecu,adresse,date_entree,tempsTravail);
         // est ce que super appelle le constructeur de Personnel qui met des valeurs par défaut à nbHeuresSupp, fixe et taux?
         // ou est ce que super appelle les variables d'instance? dans ce cas nbHeuresSupp, fixe et taux ne sont pas initialisées par super
