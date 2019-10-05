ddea8533a96a778fd45ef7be41f58b570e4aba38
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-05 14:12:58 +0200

    add tauxSoldes

--- Emplette/Article.java
+++ Emplette/Article.java
@@ -4,10 +4,12 @@ public class Article {
 	protected String nom;
 	protected float prixAchat;
 	protected float prixVente;
+	protected float tauxSoldes
 	
 	public Article(String nom, float prixAchat, float prixVente) {
 		this.nom=nom;
 		this.prixAchat=prixAchat;
 		this.prixVente=prixVente;
+		this.tauxSoldes=0;
 	}
 }
