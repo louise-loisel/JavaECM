756ddb71d1ac1174976aec8311f699483b40fda9
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-05 17:55:52 +0200

    Update Magasin.java

--- Emplette/Magasin.java
+++ Emplette/Magasin.java
@@ -1,12 +1,13 @@
 package projetjava;
 import java.util.ArrayList;
 
-public class Magasin {
+public class Magasin implements IVendrePiece, IVendreKilo{
 	protected String nom;
 	protected String adresse;
 	protected int nbVendeurs;
 	protected ArrayList<Article> articlesEnStock;
-	protected ArrayList<Integer> qteArticlesEnStock;
+//	protected ArrayList<Float> qteArticlesEnStock; 
+// la quantité en stock est rentrée comme attribut de l'article
 	protected float caisse;
 	
 	public Magasin(String nom, String adresse, int nbVendeurs, float caisse) {
@@ -15,10 +16,10 @@ public class Magasin {
 		this.nbVendeurs=nbVendeurs;
 		this.caisse=caisse;
 		this.articlesEnStock = new ArrayList<Article>();
-		this.qteArticlesEnStock = new ArrayList<Integer>();
+//		this.qteArticlesEnStock = new ArrayList<Float>();
 	}
 	
-	public void achatArticles(Article art, float qte){
+/*	public void achatArticles(Article art, float qte){
 		if art.prixAchat*qte<this.caisse{
 			articlesEnStock.add(art);
 			qteArticlesEnStock(qte);
@@ -27,8 +28,31 @@ public class Magasin {
 		else{
 			System.out.println("Fonds du magasin insuffisants");
 		}
-
+*/
 		
 	}
+
+	
+	public void vendreArticle(Article art) // argument = item de la liste d'articlesEnStock
+		if art.kiloPiece="kilo";
+			art.vendrekilo(); // devrait normalement gérer la quantité en stock si la vente a lieu
+		if art.kiloPiece="piece";
+			art.vendrePiece();
+		if art.vente; 
+			this.caisse+=art.prixVente; 
+	
+	public void retourArticle(Article art, boolean etatRetour){
+		if (art.remboursable && etatRetour;){
+			this.caisse-=art.prixVente;
+			art.ajoutQteStock(1); // on ne retourne qu'un article à la fois (vérification du bon état)
+			art.setEnStock(true);
+		}
+		else if (art.remboursable && !etatRetour){
+			this.caisse-=art.prixVente;
+		}
+		else if (!art.remboursable){
+			System.out.println("Pas de retour possible");
+		}
+	};
 	
 }
