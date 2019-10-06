d9e80156868127e93267ad5d8a90cec4848b67ed
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 22:45:23 +0200

    Update PasEntre0et100ExceptionA.java

--- Emplette/PasEntre0et100ExceptionA.java
+++ Emplette/PasEntre0et100ExceptionA.java
@@ -1,6 +1,6 @@
-package projetjava;
+package com.company.Emplette;
 
-public class PasEntre0et100ExceptionA extends Exception{
+public class PasEntre0et100ExceptionA extends RuntimeException{
 	public PasEntre0et100ExceptionA(){
 		System.out.println("Vous avez rentré un soldage incorrect");
 		  }  
