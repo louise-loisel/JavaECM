7d69c760b1d16019cc0f2b1d4c4225277e8afde6
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 20:32:05 +0200

    mef

--- EleveDigital.java
+++ EleveDigital.java
@@ -1,25 +1,25 @@
 package projetjava;
 import java.util.*;
-//Créer la classe EleveDigital qui a en plus un ArrayList pour gérer ses notes.Donner les constructeurs et les méthodes pour gérer ceci correctement.
-
 
 public class EleveDigital extends Eleve {
     
+	// -------------- ATTRIBUTS -------
 	private ArrayList<Note> listeNotes;
+	
+	// ------------ CONSTRUCTEUR
 	public EleveDigital(String nom, String prenom, String numSecu, String adresse, String ine, int promo, int annee){
 		super(nom, prenom, numSecu, ine, promo, annee, adresse);
-		this.listeNotes= new ArrayList<Note>();//a-t-on besoin du this?
+		this.listeNotes= new ArrayList<Note>();
 	}
 	
+	// ----------------- METHODES 
 	public void ajouteNote(Note n) {
-		this.listeNotes.add(n); //a-t-on besoin du this?
+		this.listeNotes.add(n);
 	}
 	
     	public ArrayList<Note> getNotes() {
-        	return this.listeNotes; // avant : "return notes", sauf que je savais pas ce que c'était que notes
+        	return this.listeNotes;
     	}
-
-
 	
 
 }
