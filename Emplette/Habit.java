58ab8debca55a53c481c567b645908b3316c6eb1
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 22:40:36 +0200

    Update Habit.java

--- Emplette/Habit.java
+++ Emplette/Habit.java
@@ -1,4 +1,4 @@
-package projetjava;
+package com.company.Emplette;
 
 public class Habit extends Article implements IPublicite, ISolde  {
     
@@ -14,30 +14,29 @@ public class Habit extends Article implements IPublicite, ISolde  {
       System.out.println("Venez vous rhabiller yes !");
     }
 
-    
-    //---- Méthodes soldes
-    
-    //connaitre taux soldes
-    public void tauxSoldes(){
-      System.out.println("Le taux de soldes est de "+(tauxSoldes*100)+"%.");
+    //**** SOLDES
+
+    public void tauxSoldes() {
+        System.out.println("Le taux de soldes est de " + (tauxSoldes * 100) + "%.");
     }
-  
-    //lancer soldes
-    public void lancerSoldes(float taux){
-      throws  PasEntre0et100ExceptionA
-    {  
-      if(0<taux<1)
-      {
-        this.tauxSoldes=taux;
-      }
-      else
-      {
-        throw new PasEntre0et100ExceptionA();
-      }
-  }
-    
-  //stopper soldes
-  public void stopSoldes(){
-    this.tauxSoldes=0;
-  }
+
+    public void lancerSoldes(float taux) throws PasEntre0et100ExceptionA {
+        if (!this.pieceKilo.equals("piece")) {
+            System.out.println("Article non éligible à un soldage car il n'est pas vendu à l'unité.");
+        } else if (0.0<taux && taux<1.0) {
+            this.tauxSoldes = taux;
+            this.enSoldes = true;
+            this.prixVente = (1 - taux) * prixVente;
+        } else {
+            throw new PasEntre0et100ExceptionA();
+        }
+    }
+
+    public void stopSoldes() {
+        this.prixVente = this.tauxSoldes * prixVente + prixVente;
+        this.enSoldes = false;
+        this.tauxSoldes = 0;
+
+    }
+
 }
