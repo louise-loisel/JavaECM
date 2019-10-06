18c1c94f055e959bbfe6041c234516135ef4bbf9
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 13:51:10 +0200

    ine : int--> String

--- Eleve.java
+++ Eleve.java
@@ -2,9 +2,10 @@ package projetjava;
 //Créer la classe Eleve qui hérite de la classe Personne qui a en plus les attributs : ine, promo, annee,
 
 public class Eleve extends Personne {
-    protected int ine, promo, annee;
+    protected int promo, annee;
+    protected String ine;
 
-    public Eleve(String nom, String prenom, String numSecu, int ine, int promo, int annee, String adresse){
+    public Eleve(String nom, String prenom, String numSecu, String ine, int promo, int annee, String adresse){
         super(nom, prenom, numSecu, adresse);
         this.ine = ine;
         this.promo=promo;
