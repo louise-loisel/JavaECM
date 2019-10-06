62769c7732d2b281777d87498eca984c00fadf8c
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 20:29:30 +0200

    MEF

--- Eleve.java
+++ Eleve.java
@@ -1,9 +1,11 @@
 package projetjava;
-//Créer la classe Eleve qui hérite de la classe Personne qui a en plus les attributs : ine, promo, annee,
 
-public class Eleve extends Personne {
+	// -------------- ATTRIBUTS -------
+    public class Eleve extends Personne {
     protected int promo, annee;
     protected String ine;
+
+    // ------------ CONSTRUCTEUR
 
     public Eleve(String nom, String prenom, String numSecu, String ine, int promo, int annee, String adresse){
         super(nom, prenom, numSecu, adresse);
