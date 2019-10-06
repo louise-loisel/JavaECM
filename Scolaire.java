34b76b759a6d5a2ee1d96b55bc4c83d3b27ad441
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 11:12:29 +0200

    Update Scolaire.java

--- Scolaire.java
+++ Scolaire.java
@@ -1,9 +1,9 @@
 package projetjava;
 
 public interface Scolaire {
-	public boolean Petit();
-	public boolean Moyen();
-	public boolean Grand();
+	public boolean petit();
+	public boolean moyen();
+	public boolean grand();
 	/*Comment aurait-on pu faire si on n’avait pas utilisé d’interface ?
 	 * En utilisant un héritage supplétaire, créant une classe abstraite de "grands"
 	 */
