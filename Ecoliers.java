b45ab198c9f1dd6de4f81d1c52392909bb549fc5
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 20:28:19 +0200

    mise en forme

--- Ecoliers.java
+++ Ecoliers.java
@@ -1,18 +1,21 @@
 package projetjava;
 
-public class Ecoliers extends Enfant implements Scolaire {// ai rajouté le "implements Scolaire"
+public class Ecoliers extends Enfant implements Scolaire {
+	
+	
+	// -------------- ATTRIBUTS -------
+	
 	private static final String niveaux[] = {"CP", "CE1", "CE2" , "CM1", "CM2"};
 	private String niveau;
-	
-	/*La classe écolier a un tableau commun à toute la classe et qui ne peut pas être modifié qui donne les cinq niveaux possibles
-	 * 
-	 *  et chaque écolier a une variable d’instance qui donne son niveau.
-	 */
-	
+
+	// ------------ CONSTRUCTEUR
+
 	public Ecoliers(String nom, String prenom, int anneeNaissance, char sexe, int deptNaissance, String adresse, int annee) {
 		super(nom, prenom, anneeNaissance, sexe, deptNaissance, adresse);
 		this.niveau=niveaux[annee];
 	}
+
+	// ----------- IMPLEMENTATION INTERFACE SCOLAIRE
 
 	public boolean petit() {
 		if (this.niveau.equals("CP"))
