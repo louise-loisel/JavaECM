0dc23e9d39a42854ccf680d71998dcccd77f5f01
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 18:09:24 +0200

    update article

--- Emplette/Article.java
+++ Emplette/Article.java
@@ -1,6 +1,8 @@
 package projetjava;
 
 public class Article implements IVendrePiece, IVendreKilo {
+
+	// ----------- ATTRIBUTS -------------
 	protected String nom;
 	protected float prixAchat;
 	protected float prixVente;
@@ -8,7 +10,11 @@ public class Article implements IVendrePiece, IVendreKilo {
 		protected float qteStock;
 		protected String pieceKilo;
 		protected boolean enStock;
-	
+		public boolean vente;
+
+	// ----------- CONSTRUCTEURS -------------
+
+	//*** constructeur par défaut
 	public Article(String nom, float prixAchat, float prixVente) {
 		this.nom=nom;
 		this.prixAchat=prixAchat;
@@ -17,10 +23,13 @@ public class Article implements IVendrePiece, IVendreKilo {
 		this.pieceKilo="défaut";
 		this.qteStock=0;
 		this.pieceKilo="default";
-		this.enStock = false};
+		this.enStock = false;
+		this.vente=false;
 	}
-	
-	// deuxième constructeur où on peut aussi préciser la quantité
+
+	//*** constructeur où on peut aussi préciser la quantité associée à l'article
+			/* pratique si on veut directement initialiser le magasin, sans avoir recours à l'achat pour tous les
+			 articles */
 	public Article(String nom, float prixAchat, float prixVente, float qte) {
 		this.nom=nom;
 		this.prixAchat=prixAchat;
@@ -28,59 +37,55 @@ public class Article implements IVendrePiece, IVendreKilo {
 		this.tauxSoldes=0;
 		this.pieceKilo="défaut";
 		this.qteStock=qte;
-		this.enStock = false};
-	}
-	
-	public float getQteStock(){return this.qteStock;}
-	public void enStock(){if this.qteStock>0;this.enStock=true}
-	
-	//---- Méthodes vente
+		this.enStock = false;}
 
-	public boolean vente();
-	
+	//---------- GETTERS ET SETTERS ------------
+
+	public float getQteStock(){return this.qteStock;}
+	public String getNom(){return this.nom;}
+
+	public void artEnStock(){this.enStock=true;}
+	public void ajoutQteStock(float ajout){this.qteStock+=ajout;}
+
+	//------------ METHODES
+
+	//**** VENTE
+
+	@Override
 	public void vendrePiece(int qteVente){
-		if (this.enStock && qteVente<this.qte Stock;)
+		if (this.enStock && qteVente<this.qteStock)
 			{this.qteStock-=qteVente;
-			System.out.println("Article vendu, il reste " + this.qteStock + " en stock.");
-			boolean vente=true;
-			}
-		else if (this.enStock && qteVente=this.qteStock;)
-			{this.qteStock=0;
-			this.enStock=false;
-			System.out.println("Article vendu, il n'en reste plus en stock.");
-			boolean vente=true;
-			}
-		else if (!this.enStock || qteVente>this.qteStock)
-			{System.out.println("Pas assez d'article en stock")
-			boolean vente=false;}
-	};
-	
-	
-	public void vendreKilo(float qteVente){
-		if (this.enStock && qteVente<this.qte Stock;)
-			{this.qteStock-=qteVente;
+			this.enStock=true;
 			System.out.println("Article vendu, il reste " + this.qteStock + " en stock.");
 			this.vente=true;
 			}
-		else if (this.enStock && qteVente=this.qteStock;)
+		else if (this.enStock && qteVente==this.qteStock)
 			{this.qteStock=0;
 			this.enStock=false;
 			System.out.println("Article vendu, il n'en reste plus en stock.");
 			this.vente=true;
 			}
 		else if (!this.enStock || qteVente>this.qteStock)
-			{System.out.println("Pas assez d'articles en stock")
+			{System.out.println("Pas assez d'article en stock");
 			this.vente=false;}
 	};
 	
-	
-	// getters & setters
-	
-	public String getNom(){return this.nom;}
-	
-	public void artEnStock(){this.enStock=true;}
-	public void ajoutQteStock(float ajout){this.qteStock+=ajout;}
-	
-	
-	
+	@Override
+	public void vendreKilo(float qteVente){
+		if (this.enStock && qteVente<this.qteStock)
+			{this.qteStock-=qteVente;
+			this.enStock=true;
+			System.out.println("Article vendu, il reste " + this.qteStock + " en stock.");
+			this.vente=true;
+			}
+		else if (this.enStock && qteVente==this.qteStock)
+			{this.qteStock=0;
+			this.enStock=false;
+			System.out.println("Article vendu, il n'en reste plus en stock.");
+			this.vente=true;
+			}
+		else if (!this.enStock || qteVente>this.qteStock)
+			{System.out.println("Pas assez d'articles en stock");
+			this.vente=false; }
+	}
 }
