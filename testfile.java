df136e68b6cdf3cacefd98ec02c6230558ae923f
ab-fnt <56152872+ab-fnt@users.noreply.github.com>
2019-10-06 14:01:01 +0200

    testfile bonne syntaxe (enseignant, ... à suivre)

--- testfile.java
+++ testfile.java
@@ -144,49 +144,53 @@ print solde alice
         // ****** Vérification des classes EleveDigi, Etudiant, Note
 
         Note maths = new Note("maths", 15, 60, true);
-        Note phys = new Note("phys", 6, false);
+        Note phys = new Note("phys", 6, 60, false);
         Note chim = new Note("chim", 10, 60, true);
 
-        Array<Note> listeNotes = {maths, phys};
+        ArrayList<Note> listeNotes = new ArrayList<Note>();
+        listeNotes.add(maths);
+        listeNotes.add(phys);
 
-        EleveDigital elevedigi = new EleveDigital("Aimée", "Boinnot", "296750000000000", "adresse", 12312341234, 2015, 2, listeNotes);
 
-        elevedigi.getNotes();
+        EleveDigital elevedigi = new EleveDigital("Aimée", "Boinnot", "296750000000000", "adresse", "12312341234", 2015, 2);
+        elevedigi.ajouteNote(maths);elevedigi.ajouteNote(phys);
+        System.out.println(elevedigi.getNotes()); // juste pour vérifier le nombre d'éléments dans la liste
         elevedigi.ajouteNote(chim);
-        elevedigi.getNotes();
+        System.out.println(elevedigi.getNotes());
+
 
         System.out.println("Est ce qu'il faut qu'on puisse récupérer d'autres attributs?");
         // pour l'instant ine, promo, annee sont en protected, ils faudrait des getters
 
         System.out.println("Nom et sexe de l'élève digi, attributs classe mère");
-        elevedigi.getNom();
-        elevedigi.getSexe();
+        System.out.println(elevedigi.getNom());
+        System.out.println(elevedigi.getSexe());
 
 
     //******* Vérification des classes Personnel, Iatos, Enseignant
-        
+
         // tentative de création d'instance de Personnel (ne devrait pas être possible)
         Personnel Kaiser = new Personnel ("Kaiser", "Soze", "1877500000", "usual suspects", "entrée 20 septembre", 150);
         System.out.println("temps travail Kaiser (personnel)?");
         Kaiser.getTempsTravail();
         System.out.println("nom Kaiser (personnel), attributs Personne?");
         Kaiser.getNom();
-        
+
         //
-        Iatos Yassine=new Iatos("Yassine", "Amiri", "1867900000", "lalaladresse", "entrée 15 septembre", 150, 47,1.2);
-        System.out.println("nom Yassine, héritage Personne = " +  Yassine.getNom());  
-        System.out.println("tempsTravail Yassine, héritage Personnelll = " +  Yassine.getTempsTravail());  
-        System.out.println("fixe Yassine, attributs défaut Iatos 1200 = " +  Yassine.getFixe()); 
-        System.out.println("taux Yassine, attributs octroyé Iatos 1.2 =" +  Yassine.getTaux()); 
-        Yassine.setTaux(1.3);
-        System.out.println("taux Yassine changé avec setTaux 1.3 = " +  Yassine.getTaux());  
+        Iatos Yassine=new Iatos("Yassine", "Amiri", "1867900000", "lalaladresse", "entrée 15 septembre", 150, 47, (float) 1.2);
+        System.out.println("nom Yassine, héritage Personne = " +  Yassine.getNom());
+        System.out.println("tempsTravail Yassine, héritage Personnelll = " +  Yassine.tempsTravail);
+        System.out.println("fixe Yassine, attributs défaut Iatos 1200 = " +  Yassine.getFixe());
+        System.out.println("taux Yassine, attributs octroyé Iatos 1.2 =" +  Yassine.getTaux());
+        Yassine.setTaux((float)1.3);
+        System.out.println("taux Yassine changé avec setTaux 1.3 = " +  Yassine.getTaux());
         Yassine.setFixe(2000);
-        System.out.println("fixe Yassine changé 2000 = " +  Yassine.getTaux());
+        System.out.println("fixe Yassine changé 2000 = " +  Yassine.getFixe());
         System.out.println("salaire Yassine = 3061,1 ? " + Yassine.calculSalaire());
-        
-        Enseignant JazzarsansDécharge=new Enseignant ("Catherine","Jazzar","17613000000","centrale marseille,"entree 2007",200,"PRAG");
+
+        Enseignant JazzarsansDécharge=new Enseignant ("Catherine","Jazzar","17613000000","centrale marseille","entree 2007",200,"PRAG");
         System.out.println("nom Jazzar, héritage Personne = " +  JazzarsansDécharge.getNom());
-        System.out.println("PRAG fixe = 2000, hminprime = 384 ? " + JazzarsansDécharge.getFixe() + ", "+ JazzarsansDécharge.gethMinPrime()); 
+        System.out.println("PRAG fixe = 2000, hminprime = 384 ? " + JazzarsansDécharge.getFixe() + ", "+ JazzarsansDécharge.gethMinPrime());
         
         
         
