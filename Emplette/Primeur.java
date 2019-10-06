705a911bba4c5e5f76ed52b21def2ec4d5cc7efd
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 23:04:46 +0200

    Comms faits

--- Emplette/Primeur.java
+++ Emplette/Primeur.java
@@ -21,19 +21,20 @@ public class Primeur extends Article implements IPublicite, ISolde  {
     }
 
     public void lancerSoldes(float taux) throws PasEntre0et100ExceptionA {
-        if (!this.pieceKilo.equals("piece")) {
+        if (!this.pieceKilo.equals("piece")) {                      //on ne solde que les produits vendus à l'unité
             System.out.println("Article non éligible à un soldage car il n'est pas vendu à l'unité.");
-        } else if (0.0<taux && taux<1.0) {
-            this.tauxSoldes = taux;
-            this.enSoldes = true;
-            this.prixVente = (1 - taux) * prixVente;
+        } else if (0.0<taux && taux<1.0) {                          //on vérifie que le taux soit entre 0 et 1
+            this.tauxSoldes = taux;                                 //on attribue un nouveau soldage
+            this.enSoldes = true;                                   //les soldes sont en cours
+            this.prixVente = (1 - taux) * prixVente;                //on attribue un nouveau prix aux articles
         } else {
-            throw new PasEntre0et100ExceptionA();
+            throw new PasEntre0et100ExceptionA();                   //catch de l'exception
         }
     }
 
-    public void stopSoldes() {
-        this.enSoldes = false;
-        this.tauxSoldes = 0;
+    public void stopSoldes() { 
+        this.prixVente = prixVente/(1-taux);                        //on rétablit le prix
+        this.enSoldes = false;                                     //les soldes ne sont plus en cours                          
+        this.tauxSoldes = 0;                                       //le taux est à 0
     }
 }
