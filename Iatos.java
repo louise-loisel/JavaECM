0bc3d596a3909db1276f7adb534a15ec8c544ce1
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 23:18:13 +0200

    Update Iatos.java

--- Iatos.java
+++ Iatos.java
@@ -1,17 +1,13 @@
-package projetjava;
+package com.company;
 
 public class Iatos extends Personnel{
-    // pas de variables déclarées ici?
+/***************CONSTRUCTEUR***********/
     public Iatos(String nom, String prenom, String numSecu, String adresse, String date_entree, int tempsTravail, float taux){
         super(nom,prenom,numSecu,adresse,date_entree,tempsTravail);
-        // est ce que super appelle le constructeur de Personnel qui met des valeurs par défaut à nbHeuresSupp, fixe et taux?
-        // ou est ce que super appelle les variables d'instance? dans ce cas nbHeuresSupp, fixe et taux ne sont pas initialisées par super
         this.taux=taux;
-        this.fixe=1200;//ne l'ai pas appelé dans le super
-        //nbHeuresSupp doit nécessairement être initialisé ici parce que le seul moyen de le calculer c'est dans Enseignant
-
+        this.fixe=1200;
     }
-
+/***************METHODE***********/
     public float calculSalaire() //au mois
     {
         float nbHeuresSupp=0;
@@ -21,7 +17,7 @@ public class Iatos extends Personnel{
         return(this.fixe*(this.tempsTravail*0.01)+nbHeuresSupp*this.taux);
     }
 
-
+/***************GETTER ET SETTER***********/
     public float getTaux(){ return this.taux;}
     public float getFixe(){ return this.fixe;}
     
