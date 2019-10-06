719ca80e0191206f113b2f2914914f4ac8ae00bc
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 23:19:28 +0200

    Update Note.java

--- Note.java
+++ Note.java
@@ -1,11 +1,12 @@
-package projetjava;
+package com.company;
 
 public class Note {
+	/***************VARIABLES***********/
 	private String nomMatiere;
 	private float note;
 	private int credits;
 	private boolean validation;
-	
+	/***************CONSTRUCTEUR***********/
 	public Note(String nomMatiere, float note, int credits, boolean validation){
 		this.nomMatiere=nomMatiere;
 		this.note=note;
