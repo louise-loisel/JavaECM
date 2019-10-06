7c364bd3d55c952aa5dee7075cb6a1f038e9c21c
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 11:16:07 +0200

    Update Lyceens.java

--- Lyceens.java
+++ Lyceens.java
@@ -1,38 +1,41 @@
-package projetjava;
+package com.company;
 
 public class Lyceens extends Enfant implements Scolaire{
 
 	//Pour un collégien les niveaux ne sont pas les mêmes et pour un lycéen non plus. Ces deux classes ont un booléen examen.
-	public String niveaux[] = {"Seconde","Premiere","Terminale"};
-	protected String niveau;
-	protected boolean examen;
+	private static String niveaux[] = {"Seconde","Premiere","Terminale"};
+	private String niveau;
+	private boolean examen;
 	
 	public Lyceens(String nom, String prenom, int anneeNaissance, char sexe, int deptNaissance, String adresse, int annee) {
 		super(nom, prenom, anneeNaissance, sexe, deptNaissance, adresse);
 		this.niveau=niveaux[annee];
-		if (this.Grand()) 
+		if (this.grand()) 
 			this.examen=true;
 		else
 			this.examen=false;
 	}
 
-	public boolean Petit() {
+	public boolean petit() {
 		 if (this.niveau.equals("Seconde"))
 			return true;
 		else 
 			return false;
 	}
 
-	public boolean Moyen() {
+	public boolean moyen() {
 		if (this.niveau.equals("Premiere"))
 			return true;
 		else 
 			return false;
 	}
-	public boolean Grand() {
+	public boolean grand() {
 		if (this.niveau.equals("Terminale"))
 			return true;
 		else 
 			return false;
+	}
+	public boolean isExamen() {
+		return examen;
 	}
 }
