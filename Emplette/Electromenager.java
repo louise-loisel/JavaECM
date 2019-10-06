0b34927eb89523ffb8892621405f5f76dd1021a1
louise-loisel <40892349+louise-loisel@users.noreply.github.com>
2019-10-06 22:40:06 +0200

    Update Electromenager.java

--- Emplette/Electromenager.java
+++ Emplette/Electromenager.java
@@ -1,4 +1,4 @@
-package projetjava;
+package com.company.Emplette;
 
 public class Electromenager extends Article implements IPublicite, ISolde {
   
@@ -12,28 +12,29 @@ public class Electromenager extends Article implements IPublicite, ISolde {
   public void slogan(){
     System.out.println("Venez vous équipez pour la maison yes !");
   }
-  
-  //connaitre taux soldes
-  public void tauxSoldes(){
-    System.out.println("Le taux de soldes est de "+(tauxSoldes*100)+"%.");
-  }
-  
-  //lancer soldes
-  public void lancerSoldes(float taux){
-    throws  PasEntre0et100ExceptionA
-  {  
-      if(0<taux<1)
-      {
-        this.tauxSoldes=taux;
-      }
-      else
-      {
-        throw new PasEntre0et100ExceptionA();
-      }
-  }
-    
-  //stopper soldes
-  public void stopSoldes(){
-    this.tauxSoldes=0;
-  }
+
+    //**** SOLDES
+    @Override
+    public void tauxSoldes() {
+        System.out.println("Le taux de soldes est de " + (tauxSoldes * 100) + "%.");
+    }
+    @Override
+    public void lancerSoldes(float taux) throws PasEntre0et100ExceptionA {
+        if (!this.pieceKilo.equals("piece")) {
+            System.out.println("Article non éligible à un soldage car il n'est pas vendu à l'unité.");
+        }
+        else if (0.0<taux && taux<1.0) {
+            this.tauxSoldes = taux;
+            this.enSoldes = true;
+            this.prixVente = (1 - taux) * prixVente;
+        }
+        else {
+            throw new PasEntre0et100ExceptionA();
+        }
+    }
+    @Override
+    public void stopSoldes() {
+        this.enSoldes = false;
+        this.tauxSoldes = 0;
+    }
 }
